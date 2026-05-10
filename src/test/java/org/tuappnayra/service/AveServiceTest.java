aafdb73c0232ac1421f2030ea67d3f9dfd952aff
NRJ029 <nayrarj2006@gmail.com>
2026-05-10 18:11:41 +0100

    test: cambio valor esperado para verificar no fallo en CI

--- src/test/java/org/tuappnayra/service/AveServiceTest.java
+++ src/test/java/org/tuappnayra/service/AveServiceTest.java
@@ -41,7 +41,7 @@ public class AveServiceTest {
         Ave guardado = service.guardarAve(new Ave());
 
         assertNotNull(guardado);
-        assertEquals("Fallar test", guardado.getNombre());
+        assertEquals("Periquito", guardado.getNombre());
         verify(repository, times(1)).save(any(Ave.class));
     }
 
